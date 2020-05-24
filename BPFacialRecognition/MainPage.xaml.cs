da376d54c4886271ba3d1956d6b2cb3d51cc2dee
Waut Wyffels <waut.wyffels@outlook.com>
2020-05-24 14:45:33 +0200

    Added face tracking to camera feed

--- BPFacialRecognition/MainPage.xaml.cs
+++ BPFacialRecognition/MainPage.xaml.cs
@@ -7,19 +7,40 @@ using Microsoft.ProjectOxford.Face;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
 using System.Threading.Tasks;
 
+using Windows.Graphics.Imaging;
+using Windows.Media;
+using Windows.Media.Capture;
+using Windows.Media.FaceAnalysis;
+using Windows.Media.MediaProperties;
 using Windows.Storage;
 using Windows.System.Display;
+using Windows.System.Threading;
 using Windows.UI.Core;
 using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 namespace BPFacialRecognition {
     public sealed partial class MainPage : Page {
+
+        #region Face Tracking
+
+        private readonly SolidColorBrush lineBrush = new SolidColorBrush(Windows.UI.Colors.Yellow);
+        private readonly double lineThickness = 2.0;
+        private readonly SolidColorBrush fillBrush = new SolidColorBrush(Windows.UI.Colors.Transparent);
+
+        private VideoEncodingProperties videoProperties;
+        private FaceTracker faceTracker;
+        private ThreadPoolTimer frameProcessingTimer;
+        private SemaphoreSlim frameProcessingSemaphore = new SemaphoreSlim(1);
+        #endregion
 
         private DisplayRequest displayRequest = new DisplayRequest();
 
@@ -66,9 +87,12 @@ namespace BPFacialRecognition {
         /// <summary>
         /// Triggered every time the page is navigated to.
         /// </summary>
-        protected override void OnNavigatedTo(NavigationEventArgs e) {
+        protected override async void OnNavigatedTo(NavigationEventArgs e) {
             if (this.initializedFaceApi)
                 UpdateWhitelistedVisitors();
+
+            if (this.faceTracker == null)
+                this.faceTracker = await FaceTracker.CreateAsync();
         }
 
         /// <summary>
@@ -89,12 +113,23 @@ namespace BPFacialRecognition {
                 await this.camera.InitializeCameraAsync();
             }
 
-            if (CanPreview) {
-                WebcamFeed.Source = this.camera.MediaCapture;
-                await this.camera.StartCameraPreview();
+            if (CanPreview)
+                await StartPreviewAsync();
+        }
 
-                this.displayRequest.RequestActive();
-            }
+        private async Task StartPreviewAsync() {
+            // Cache the media properties as we'll need them later.
+            var deviceController = this.camera.MediaCapture.VideoDeviceController;
+            this.videoProperties = deviceController.GetMediaStreamProperties(MediaStreamType.VideoPreview) as VideoEncodingProperties;
+
+            WebcamFeed.Source = this.camera.MediaCapture;
+            await this.camera.StartCameraPreview();
+
+            TimeSpan timerInterval = TimeSpan.FromMilliseconds(66);
+            this.frameProcessingTimer = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(ProcessCurrentVideoFrame), timerInterval);
+
+
+            this.displayRequest.RequestActive();
         }
 
         /// <summary>
@@ -270,5 +305,91 @@ namespace BPFacialRecognition {
             // Exit app
             Application.Current.Exit();
         }
+
+        #region Face TRacking
+
+
+
+        /// <summary>
+        /// This method is invoked by a ThreadPoolTimer to execute the FaceTracker and Visualization logic at approximately 15 frames per second.
+        /// </summary>
+        /// <remarks>
+        /// Keep in mind this method is called from a Timer and not synchronized with the camera stream. Also, the processing time of FaceTracker
+        /// will vary depending on the size of each frame and the number of faces being tracked. That is, a large image with several tracked faces may
+        /// take longer to process.
+        /// </remarks>
+        /// <param name="timer">Timer object invoking this call</param>
+        private async void ProcessCurrentVideoFrame(ThreadPoolTimer timer) {
+            // If a lock is being held it means we're still waiting for processing work on the previous frame to complete.
+            // In this situation, don't wait on the semaphore but exit immediately.
+            if (!frameProcessingSemaphore.Wait(0)) {
+                return;
+            }
+
+            try {
+                IList<DetectedFace> faces = null;
+
+                // Create a VideoFrame object specifying the pixel format we want our capture image to be (NV12 bitmap in this case).
+                // GetPreviewFrame will convert the native webcam frame into this format.
+                const BitmapPixelFormat InputPixelFormat = BitmapPixelFormat.Nv12;
+                using (VideoFrame previewFrame = new VideoFrame(InputPixelFormat, (int)this.videoProperties.Width, (int)this.videoProperties.Height)) {
+                    await this.camera.MediaCapture.GetPreviewFrameAsync(previewFrame);
+
+                    // The returned VideoFrame should be in the supported NV12 format but we need to verify this.
+                    if (FaceDetector.IsBitmapPixelFormatSupported(previewFrame.SoftwareBitmap.BitmapPixelFormat)) {
+                        faces = await this.faceTracker.ProcessNextFrameAsync(previewFrame);
+                    }
+                    else {
+                        throw new System.NotSupportedException("PixelFormat '" + InputPixelFormat.ToString() + "' is not supported by FaceDetector");
+                    }
+
+                    // Create our visualization using the frame dimensions and face results but run it on the UI thread.
+                    var previewFrameSize = new Windows.Foundation.Size(previewFrame.SoftwareBitmap.PixelWidth, previewFrame.SoftwareBitmap.PixelHeight);
+                    var ignored = this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => {
+                        this.SetupVisualization(previewFrameSize, faces);
+                    });
+                }
+            }
+            catch (Exception ex) {
+                Debug.WriteLine(ex);
+            }
+            finally {
+                frameProcessingSemaphore.Release();
+            }
+
+        }
+
+        /// <summary>
+        /// Takes the webcam image and FaceTracker results and assembles the visualization onto the Canvas.
+        /// </summary>
+        /// <param name="framePizelSize">Width and height (in pixels) of the video capture frame</param>
+        /// <param name="foundFaces">List of detected faces; output from FaceTracker</param>
+        private void SetupVisualization(Windows.Foundation.Size framePizelSize, IList<DetectedFace> foundFaces) {
+            this.VisualizationCanvas.Children.Clear();
+
+            double actualWidth = this.VisualizationCanvas.ActualWidth;
+            double actualHeight = this.VisualizationCanvas.ActualHeight;
+
+            if (foundFaces != null && actualWidth != 0 && actualHeight != 0) {
+                double widthScale = framePizelSize.Width / actualWidth;
+                double heightScale = framePizelSize.Height / actualHeight;
+
+                foreach (DetectedFace face in foundFaces) {
+                    // Create a rectangle element for displaying the face box but since we're using a Canvas
+                    // we must scale the rectangles according to the frames's actual size.
+                    Rectangle box = new Rectangle();
+                    box.Width = (uint)(face.FaceBox.Width / widthScale);
+                    box.Height = (uint)(face.FaceBox.Height / heightScale);
+                    box.Fill = this.fillBrush;
+                    box.Stroke = this.lineBrush;
+                    box.StrokeThickness = this.lineThickness;
+                    box.Margin = new Thickness((uint)(face.FaceBox.X / widthScale), (uint)(face.FaceBox.Y / heightScale), 0, 0);
+
+                    this.VisualizationCanvas.Children.Add(box);
+                }
+            }
+        }
+
+        #endregion
     }
 }
